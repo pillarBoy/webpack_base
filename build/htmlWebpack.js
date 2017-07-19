1573c48880beda133898c05e1e59a308cbcf80c7
pillar <601991736@qq.com>
2017-07-19 11:39:25 +0800

    修复在模板html内部写文件路径，路径不对问题

--- build/htmlWebpack.js
+++ build/htmlWebpack.js
@@ -1,4 +1,5 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); // 处理html中img src 正确引入图片路径
 var config = require('../config');
 
 function mkHtml(pageList, env) {
@@ -9,7 +10,7 @@ function mkHtml(pageList, env) {
   pageList.forEach(function(pageName) {
     let option = env === 'production' ? { // 根据模板插入css/js等生成最终HTML
       favicon: './src/assets/favicon.ico', // favicon路径，通过webpack引入同时可以生成hash值
-      filename: `./${pageName}.html`, // 生成的html存放路径，相对于path
+      filename: `html-withimg-loader!` + path.resolve(`./src/views/`, `${pageName}.html`), // 生成的html存放路径，相对于path
       template: `./src/views/${pageName}.html`, // html模板路径
       inject: true, // js插入的位置，true/'head'/'body'/false
       hash: true, // 为静态资源生成hash值
@@ -21,7 +22,7 @@ function mkHtml(pageList, env) {
     } : {
       filename: `./${pageName}.html`,
       favicon: './src/assets/favicon.ico',
-      template: `./src/views/${pageName}.html`,
+      template: `html-withimg-loader!` + path.resolve(`./src/views/`, `${pageName}.html`), // html-withimg-loader 处理html内部写img 匹配对src路径
       chunks: ['vendors', pageName], // 需要引入的chunk，不配置就会引入所有页面的资源
       inject: true
     };
@@ -29,6 +30,4 @@ function mkHtml(pageList, env) {
   });
   return pagesHtmls;
 }
-// let arr = ['app', 'list'];
-// console.log(mkHtml(arr));
 module.exports = mkHtml;
